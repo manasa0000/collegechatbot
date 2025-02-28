fadc8fb20e755682272bcad0c947713b8d29a6c9
manasa0000 <manasavabilisetti66@gmail.com>
2025-02-28 10:39:07 +0530

    Create college.py

---
+++ college.py
@@ -0,0 +1,5 @@
+import streamlit as st
+import pandas as pd
+import google.generativeai as genai
+from sklearn.metrics.pairwise import cosine.similarity
+from sklearn.feature_extension.text import TfidfVectorizer
